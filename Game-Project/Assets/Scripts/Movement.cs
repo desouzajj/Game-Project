342819890b7523eb185d094b111fbef0b2e657aa
Jacob DeSouza <jacobdesouza148@yahoo.com>
2023-09-19 13:24:32 -0400

    update

--- Game-Project/Assets/Scripts/Movement.cs
+++ Game-Project/Assets/Scripts/Movement.cs
@@ -23,7 +23,7 @@ public class Movement : MonoBehaviour
 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 direction = new Vector3(-horizontal, 0f, vertical).normalized;
 
         if(direction.magnitude >= 0.1f)
         {
